c753f5f9da793ffd22c854fb88c69c8ea00eed03
hari vasudev <hari_vasudev@yahoo.com>
2020-08-10 16:22:13 +0530

    final commit

--- src/component/spa/contentcontainer/home/home.js
+++ src/component/spa/contentcontainer/home/home.js
@@ -1,51 +1,108 @@
 import React from 'react';
-import { Chart } from 'react-google-charts'
+import { Chart,Pie } from 'react-chartjs-2';
 import axios from 'axios';
 
+
 class Home extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-        stockList:{}
-        
-        
+  constructor(props){
+    super(props);
+    this.state={
+       
+        selectedData:[],
+        chartData:{    
+        }
     }
 }
-componentWillMount() {
-  this.stockData()
-}
 
-stockData = () => {
-  axios.get('http://localhost:3000/allproducts')
-      .then(response => {
-          console.log(response.data)
-          this.setState({ stockList: response.data.stock })
-      }, error => { console.error(error) })
+componentWillMount(){
   
+    if(localStorage.getItem('loggedIn') === null){
+        this.props.history.push('/');
+    }
+}
+componentDidMount() {
+  this.getChartData();
 }
 
-render() {
+getChartData() {
+  
+    axios.get('http://localhost:3000/allProducts').then((res)=>{
+        let titles=[]
+        var stocks=[]
+        console.log(res.data)
+        this.setState({selectedData:res.data})
+        this.state.selectedData.map((cat)=>{
+           return(
+            titles.push(cat.name)
+           ) 
+        });
+        this.state.selectedData.map((cat)=>{
+            return(
+             stocks.push(parseInt(cat.stock))
+             
+            ) 
+         });
+         console.log(titles)
+         console.log("stocks"+stocks)
+         this.setState({chartData:
+            {
+                labels:[...titles],
+            datasets:[{
+                label:'Stock Availability',
+                data:[
+                    stocks
+                ],
+                backgroundColor:[
+                    'rgba(225,99,132,0.6)',
+                    'rgba(225,206,86,0.6)',
+                    'rgba(54,162,235,0.6)',
+                    'rgba(75,192,192,0.6)',
+                    'rgba(153,102,255,0.6)',
+                    'rgba(225,159,64,0.6)',
+                    'rgba(225,99,132,0.6)'
+                ]
+            }],
+            } 
+        });
+    })
+    
+    
+    console.log(this.state.chartData)
+}
+render() { 
+    return ( 
+        <div className="dashboard">
+            <div>
+              
+                
+            </div>
+            <div>
+            {/* {Object.keys(this.state.chartData).length &&
+            <Chart
+              chartData={this.state.chartData}
+              location="Massachusetts"
+              legendPosition="bottom"
+            />
+          } */}
+                <h1>Chart</h1>
+                <Pie
+                data={this.state.chartData}
+                height={80}
+                options={{
+                    title:{
+                        display:true,
+                        text:'Stock Availability'
+                    },
+                    legend:{
+                        display:true,
+                        position:"top"
+                    }
+                }}>
 
-  return (
-      <span className="chart">
-          <Chart
-              width='500px'
-              height='270px'
-              chartType="Bar"
-              loader={<span>Loading Chart</span>}
-              data={[
-                  ['Stock',this.state.stockList]
-               
-              ]}
-              options={{
-                  chart: {
-                      title: 'Stock of each products',
-                     
-                  },
-              }}
-          />
-      </span>
-  );
+                </Pie>
+            </div>
+        </div>
+     );
 }
 }
 export default Home;
